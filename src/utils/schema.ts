0d8cb398c29a7d52d2f9c00cef08a0c0241476f8
mezz01 <mezzatamine2002@gmail.com>
2025-02-10 11:39:01 +0100

    adding schema

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -76,3 +76,30 @@ export function generateSchema(location) {
       }
     };
   }
+
+
+  export function generateSchema(location: string) {
+    return {
+      "@context": "http://schema.org",
+      "@type": "Product",
+      name:  `IPTV Kopen in ${location} | Beste IPTV Kastje en Abonnementen`,
+      brand: "IPTV Kopen",
+      description: `Op zoek naar de beste IPTV-abonnementen en IPTV kastjes in ${location}? Bij iptv-kastje-kopen.nl vind je de nieuwste aanbiedingen voor 2025. Vergelijk providers en geniet van legale IPTV in ${location}.`,
+      keywords: ["iptv kopen", "iptv kastje", "iptv box", "iptv nederland"],
+      aggregateRating: {
+        "@type": "AggregateRating",
+        ratingValue: "9.9",
+        ratingCount: "5",
+        bestRating: "10",
+        worstRating: "1",
+      },
+      offers: {
+        "@type": "aggregateOffer",
+        availability: "http://schema.org/InStock",
+        priceCurrency: "EUR",
+        lowPrice: "11.99",
+        offerCount: "159",
+        highPrice: "59",
+      },
+    };
+  }
